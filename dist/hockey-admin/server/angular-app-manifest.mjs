06e81381a8296fe8750f4f19ed59e7579b639ebb
RayHylian <boughelam.rachid@hotmail.fr>
2025-04-15 08:39:09 -0300

    building project

--- dist/hockey-admin/server/angular-app-manifest.mjs
+++ dist/hockey-admin/server/angular-app-manifest.mjs
@@ -29,12 +29,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5527, hash: 'cbe44e64cf1b3d9b856bc2e56cd871837fa1daaf72831bb9d35c026b67527544', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1008, hash: '47170e3369eb3662434e39834b37a2b1c371be31106a027e43e4d976e1b83eed', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 7475, hash: '454625e0c130e80b624baebbbf91af449c0a2d633923d139fc672f201c9f6cf3', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
-    'team/index.html': {size: 5847, hash: 'e478d50df40442796c952994f8dba7d788fe6e2a8688c6fe09038096d37fcc7d', text: () => import('./assets-chunks/team_index_html.mjs').then(m => m.default)},
-    'result/index.html': {size: 5853, hash: '2283692d5f01bfd0a65ee601e853b5b4383cfdd53d43d797108e9d628ce5232e', text: () => import('./assets-chunks/result_index_html.mjs').then(m => m.default)},
-    'article/index.html': {size: 11318, hash: '511e56c901e4f2975bf1e379a535a1d3b05474c7f29b7f4361e9342ce4fe30e6', text: () => import('./assets-chunks/article_index_html.mjs').then(m => m.default)},
-    'styles-VCVKDVAX.css': {size: 13044, hash: 'aT6fY0vM16k', text: () => import('./assets-chunks/styles-VCVKDVAX_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5527, hash: 'd2f3cb57f72be0ab0d31b4169e1120e12b80b21fdb2cdf634ea75add345046c1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1008, hash: '16c8099632e1c0e53eaccd87f6c0a52b60f17cbe887332ea33278f427a3e222c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 7475, hash: 'ae29d41748c30029ea5f08441c6292afa0fa08850d160aad295b752422415fb3', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'result/index.html': {size: 5853, hash: 'df36ca0ca402efec25824e31eeada612ba98d85b514b903cbc57e06b0ff00632', text: () => import('./assets-chunks/result_index_html.mjs').then(m => m.default)},
+    'team/index.html': {size: 5847, hash: 'c25210eea68077496858dabed5548160e619513e5a0f8f8dba1891b897c92324', text: () => import('./assets-chunks/team_index_html.mjs').then(m => m.default)},
+    'article/index.html': {size: 11318, hash: '639c801363afd42d560d3999f2159788fb225b2b7d76e6460ae87648dde71104', text: () => import('./assets-chunks/article_index_html.mjs').then(m => m.default)},
+    'styles-HPC5DEEO.css': {size: 13077, hash: '00EofyU7TZI', text: () => import('./assets-chunks/styles-HPC5DEEO_css.mjs').then(m => m.default)}
   },
 };
